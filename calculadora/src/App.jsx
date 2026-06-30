d9dbe67463959f6f6f3c45d57890a35b52b714bd
flameastro <gabrielgoulartbnu@gmail.com>
2026-06-30 15:41:50 -0300

    cria função addChar e useState

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react'
 import './App.css'
 import Resultado from './components/Resultado'
 
 function App() {
-  return (
-    <div>
-      <h1>Calculadora</h1>
-      <Resultado></Resultado>
-    </div>
-  )
+    const [res, setRes] = useState("")
+
+    function addChar(char) {
+        setRes((prevRes) => prevRes + char)
+    }
+
+    return (
+        <div>
+            <h1>Calculadora</h1>
+            <button onClick={() => addChar("0")}>0</button>
+            <button onClick={() => addChar("1")}>1</button>
+            <button onClick={() => addChar("2")}>2</button>
+            <button onClick={() => addChar("3")}>3</button>
+            <button onClick={() => addChar("4")}>4</button>
+            <button onClick={() => addChar("5")}>5</button>
+            <button onClick={() => addChar("6")}>6</button>
+            <button onClick={() => addChar("7")}>7</button>
+            <button onClick={() => addChar("8")}>8</button>
+            <button onClick={() => addChar("9")}>9</button>
+            <Resultado res={res}></Resultado>
+        </div>
+    )
 }
 
 export default App
