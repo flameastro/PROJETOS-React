277fc57f3e9532d46fee61f1ed6b1cd6018aa55b
flameastro <gabrielgoulartbnu@gmail.com>
2026-07-01 13:55:20 -0300

    fetch(Input): adiciona funcionalidade de enviar mensagens

--- chatbot/components/Input.jsx
+++ chatbot/components/Input.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react"
+import Message from "./Message.jsx"
+
 function Input({ placeholder, btnText }) {
+    let [messages, setMessages] = useState([])
+    let [text, setText] = useState("")
+
+    function handleInput(char) {
+        setText(() => char)
+    }
+
+    function sendMessage() {
+        setMessages((prevMessages) => [...prevMessages, text])
+        console.log(messages)
+    }
+
     return (
         <div>
-            <input type="text" name="input" id="input" placeholder={placeholder} />
-            <button>{btnText}</button>
+            <input type="text" name="input" id="input"
+                placeholder={placeholder}
+                value={text}
+                onChange={(event) => handleInput(event.target.value)} />
+            <button onClick={() => sendMessage()}>{btnText}</button>
+
+            {messages.map((message, id) => (
+                <Message key={id} prompt={message}></Message>
+            ))}
         </div>
     )
 }
--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,12 +1,13 @@
 import Input from "../components/Input"
-import Message from "../components/Message"
 
 function App() {
     return (
         <div>
             <h1>ChatBot</h1>
-            <Input placeholder={"Send a Message to ChatBot"} btnText={"Send"}></Input>
-            <Message prompt={"test"}></Message>
+            <Input
+                placeholder={"Send a Message to ChatBot"}
+                btnText={"Send"}
+            ></Input>
         </div>
     )
 }
